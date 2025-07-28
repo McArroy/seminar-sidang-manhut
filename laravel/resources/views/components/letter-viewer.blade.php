a67fec0eb79c15203e2681e6bd5565b02c764a88
McArroy <132247939+McArroy@users.noreply.github.com>
2025-07-28 15:58:41 +0700

    Added letter-viewer element

---
+++ laravel/resources/views/components/letter-viewer.blade.php
@@ -0,0 +1,15 @@
+@props(["id" => "pdf-source", "title" => "PDF Viewer", "sandbox" => ""])
+
+<div class="letter-viewer">
+	<div class="top">
+		<p>{{ $title }}</p>
+		<div class="buttons">
+			<x-button class="icon-fullscreen" icon="material-symbols-light:pan-zoom" iconwidth="25" onclick="return FullScreenPDFFrame($('.icon-fullscreen iconify-icon'), $('.letter-viewer'));"></x-button>
+			<x-button icon="carbon:zoom-reset" iconwidth="25" onclick="return ZoomResetPDFFrame($('#{{ $id }}'));"></x-button>
+			<x-button icon="iconamoon:zoom-out-light" iconwidth="25" onclick="return ZoomOutPDFFrame($('#{{ $id }}'));"></x-button>
+			<x-button icon="iconamoon:zoom-in-light" iconwidth="25" onclick="return ZoomInPDFFrame($('#{{ $id }}'));"></x-button>
+			<x-button icon="mynaui:download" iconwidth="25" onclick="return DownloadPDF($('#{{ $id }}'));"></x-button>
+		</div>
+	</div>
+	<iframe id="{{ $id }}" class="viewer" sandbox="{{ $sandbox }}" onload="return ApplyZoomPDFFrame($('#{{ $id }}'));" srcdoc="{{ $slot }}"></iframe>
+</div>
