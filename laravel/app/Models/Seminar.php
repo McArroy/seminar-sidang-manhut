d4b5239619e152d30ba02d714b5f8f8e6681bb1e
McArroy <132247939+McArroy@users.noreply.github.com>
2025-07-29 15:09:17 +0700

    Updated Seminar model by enabling encrypted data

--- laravel/app/Models/Seminar.php
+++ laravel/app/Models/Seminar.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Crypt;
 
 class Seminar extends Model
 {
@@ -22,7 +23,54 @@ class Seminar extends Model
 		"time",
 		"place",
 		"title",
+		"link",
 		"comment",
 		"status"
 	];
+
+	// List of attributes to encrypt
+	protected $encrypted =
+	[
+		"useridnumber",
+		"studyprogram",
+		"department",
+		"supervisor1",
+		"supervisor2",
+		"date",
+		"time",
+		"place",
+		"title",
+		"link",
+		"comment"
+	];
+
+	// Encrypt values before saving
+	public function setAttribute($key, $value)
+	{
+		if (in_array($key, $this->encrypted) && $value !== null)
+			$value = Crypt::encryptString($value);
+
+		return parent::setAttribute($key, $value);
+	}
+	
+	// Decrypt values when accessing
+	public function getAttribute($key)
+	{
+		$value = parent::getAttribute($key);
+
+		if (in_array($key, $this->encrypted) && $value !== null)
+		{
+			try
+			{
+				return Crypt::decryptString($value);
+			}
+			catch (\Exception $e)
+			{
+				// optionally log: corrupted or already-decrypted value
+				return $value;
+			}
+		}
+
+		return $value;
+	}
 }
