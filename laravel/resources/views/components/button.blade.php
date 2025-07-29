4005439a6f66d13bad81920d6e1f7b94bc4842b7
McArroy <132247939+McArroy@users.noreply.github.com>
2025-07-29 15:13:52 +0700

    Added "href" and "target" attribute on button element that acts like from tag <a>

--- laravel/resources/views/components/button.blade.php
+++ laravel/resources/views/components/button.blade.php
@@ -1,13 +1,14 @@
-@props(["icon" => null, "iconclass" => "", "iconwidth" => ""])
+@props(["href" => null, "target" => "_self", "icon" => null, "iconclass" => "", "iconwidth" => ""])
 
 @php
 $classes = "button";
-$iconHTML = ($icon ?? false)
-	? "<iconify-icon icon='{$icon}' class='{$iconclass}' width='{$iconwidth}'></iconify-icon>"
-	: "";
+$onclick = $href ? "window.open('{$href}', '{$target}')" : null;
 @endphp
 
-<button {{ $attributes->merge(["class" => $classes]) }}>
-	{!! $iconHTML !!}
+<button {{ $attributes->merge(["class" => $classes]) }} @if ($onclick) onclick="{{ $onclick }}" @endif>
+	@if ($icon)
+	<iconify-icon icon="{{ $icon }}" class="{{ $iconclass }}" width="{{ $iconwidth }}"></iconify-icon>
+	@endif
+
 	{{ $slot }}
 </button>
