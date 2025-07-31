2b0f459f642a84e5a544831ac2ec6efd4a221558
McArroy <132247939+McArroy@users.noreply.github.com>
2025-07-31 14:08:02 +0700

    Added Thesisdefense model

---
+++ laravel/app/Models/Thesisdefense.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Crypt;
+
+class Thesisdefense extends Model
+{
+	protected $primaryKey = "thesisdefenseid";
+	public $incrementing = false;
+	protected $keyType = "string";
+
+	protected $fillable =
+	[
+		"thesisdefenseid",
+		"useridnumber",
+		"semester",
+		"address",
+		"supervisor1",
+		"supervisor2",
+		"date",
+		"time",
+		"place",
+		"title",
+		"link",
+		"comment",
+		"status"
+	];
+
+	// List of attributes to encrypt
+	protected $encrypted =
+	[
+		"useridnumber",
+		"semester",
+		"address",
+		"supervisor1",
+		"supervisor2",
+		"date",
+		"time",
+		"place",
+		"title",
+		"link",
+		"comment"
+	];
+
+	// Encrypt values before saving
+	public function setAttribute($key, $value)
+	{
+		if (in_array($key, $this->encrypted) && $value !== null)
+			$value = Crypt::encryptString($value);
+
+		return parent::setAttribute($key, $value);
+	}
+	
+	// Decrypt values when accessing
+	public function getAttribute($key)
+	{
+		$value = parent::getAttribute($key);
+
+		if (in_array($key, $this->encrypted) && $value !== null)
+		{
+			try
+			{
+				return Crypt::decryptString($value);
+			}
+			catch (\Exception $e)
+			{
+				// optionally log: corrupted or already-decrypted value
+				return $value;
+			}
+		}
+
+		return $value;
+	}
+}
