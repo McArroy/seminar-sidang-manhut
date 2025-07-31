b257918c0be38b9351c23800fe288ea48be67dee
McArroy <132247939+McArroy@users.noreply.github.com>
2025-07-31 14:25:53 +0700

    Fixed users are not redirected to the desired dashboard

--- laravel/app/Providers/FortifyServiceProvider.php
+++ laravel/app/Providers/FortifyServiceProvider.php
@@ -8,41 +8,72 @@ use App\Actions\Fortify\UpdateUserPassword;
 use App\Actions\Fortify\UpdateUserProfileInformation;
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Str;
 use Laravel\Fortify\Actions\RedirectIfTwoFactorAuthenticatable;
+use Laravel\Fortify\Contracts\LoginResponse;
+use Laravel\Fortify\Contracts\LogoutResponse;
 use Laravel\Fortify\Fortify;
+
 
 class FortifyServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
-    public function register(): void
-    {
-        //
-    }
+	/**
+	 * Register any application services.
+	 */
+	public function register() : void
+	{
+		//
+	}
 
-    /**
-     * Bootstrap any application services.
-     */
-    public function boot(): void
-    {
-        Fortify::createUsersUsing(CreateNewUser::class);
-        Fortify::updateUserProfileInformationUsing(UpdateUserProfileInformation::class);
-        Fortify::updateUserPasswordsUsing(UpdateUserPassword::class);
-        Fortify::resetUserPasswordsUsing(ResetUserPassword::class);
-        Fortify::redirectUserForTwoFactorAuthenticationUsing(RedirectIfTwoFactorAuthenticatable::class);
+	/**
+	 * Bootstrap any application services.
+	 */
+	public function boot() : void
+	{
+		Fortify::createUsersUsing(CreateNewUser::class);
+		Fortify::updateUserProfileInformationUsing(UpdateUserProfileInformation::class);
+		Fortify::updateUserPasswordsUsing(UpdateUserPassword::class);
+		Fortify::resetUserPasswordsUsing(ResetUserPassword::class);
+		Fortify::redirectUserForTwoFactorAuthenticationUsing(RedirectIfTwoFactorAuthenticatable::class);
 
-        RateLimiter::for('login', function (Request $request) {
-            $throttleKey = Str::transliterate(Str::lower($request->input(Fortify::username())).'|'.$request->ip());
+		RateLimiter::for("login", function(Request $request)
+		{
+			$throttleKey = Str::transliterate(Str::lower($request->input(Fortify::username())) . "|" . $request->ip());
 
-            return Limit::perMinute(5)->by($throttleKey);
-        });
+			return Limit::perMinute(5)->by($throttleKey);
+		});
 
-        RateLimiter::for('two-factor', function (Request $request) {
-            return Limit::perMinute(5)->by($request->session()->get('login.id'));
-        });
-    }
+		RateLimiter::for("two-factor", function(Request $request)
+		{
+			return Limit::perMinute(5)->by($request->session()->get("login.id"));
+		});
+
+		$this->app->singleton(LoginResponse::class, function()
+		{
+			return new class implements LoginResponse
+			{
+				public function toResponse($request)
+				{
+					if (Auth::user()->userrole === "admin")
+						return redirect()->intended(route("admin.dashboard"));
+				
+					return redirect()->intended(route("student.dashboard"));
+				}
+			};
+		});
+
+		$this->app->singleton(LogoutResponse::class, function()
+		{
+			return new class implements LogoutResponse
+			{
+				public function toResponse($request)
+				{
+					return redirect("/login");
+				}
+			};
+		});
+	}
 }
