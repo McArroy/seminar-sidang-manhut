75919748fbbbc66607b6c7304c6685ba12c7230f
McArroy <132247939+McArroy@users.noreply.github.com>
2025-07-31 14:20:17 +0700

    Added "oninput", "onchange", and "autofocus" attribute on input element and fixed some bugs when element added a new slot inside it

--- laravel/resources/views/components/input-wrapper.blade.php
+++ laravel/resources/views/components/input-wrapper.blade.php
@@ -1,4 +1,4 @@
-@props(["id" => "input-name", "label" => null, "type" => "text", "placeholder" => "", "value" => "", "options" => [], "readonly" => false, "required" => false])
+@props(["id" => "input-name", "label" => null, "type" => "text", "placeholder" => "", "value" => "", "options" => [], "oninput" => null, "onchange" => null, "readonly" => false, "required" => false, "autofocus" => false])
 
 <div {{ $attributes->merge(["class" => "input-wrapper"]) }}>
 	@if ($label)
@@ -6,17 +6,25 @@
 	@endif
 
 	@if ($type === "textarea" || $type === "url")
-	<textarea id="{{ $id }}" name="{{ $id }}" placeholder="{{ $placeholder }}" @if($readonly) readonly @endif @if($required) required @endif>{{ $value }}</textarea>
+	<textarea id="{{ $id }}" name="{{ $id }}" placeholder="{{ $placeholder }}" oninput="{{ $oninput }}" onchange="{{ $onchange }}" @if($readonly) readonly @endif @if($required) required @endif @if($autofocus) autofocus @endif>{{ $value }}</textarea>
 	@elseif ($type === "select")
-	<select name="{{ $id }}" id="{{ $id }}" @if($readonly) disabled @endif @if($required) required @endif>
+	<select name="{{ $id }}" id="{{ $id }}" oninput="{{ $oninput }}" onchange="{{ $onchange }}" @if($readonly) disabled @endif @if($required) required @endif @if($autofocus) autofocus @endif>
 		<option value="" disabled selected hidden>{{ $placeholder ?: "Pilih" }}</option>
+		@if (!empty($options))
 			@foreach ($options as $optionValue => $optionLabel)
 				<option value="{{ $optionValue }}" @if($value == $optionValue) selected @endif>
 					{{ $optionLabel }}
 				</option>
 			@endforeach
+		@else
+			{{ $slot }}
+		@endif
 	</select>
 	@else
-	<input type="{{ $type }}" id="{{ $id }}" name="{{ $id }}" placeholder="{{ $placeholder }}" value="{{ $value }}" @if($readonly) readonly @endif @if($required) required @endif>
+	<input type="{{ $type }}" id="{{ $id }}" name="{{ $id }}" placeholder="{{ $placeholder }}" oninput="{{ $oninput }}" onchange="{{ $onchange }}" value="{{ $value }}" @if($readonly) readonly @endif @if($required) required @endif @if($autofocus) autofocus @endif>
+	@endif
+
+	@if ($type !== "select" && empty($options))
+		{{ $slot }}
 	@endif
 </div>
