f57657c597c71fa578ee00495991ae713c9c17f5
McArroy <132247939+McArroy@users.noreply.github.com>
2025-08-04 16:08:34 +0700

    Fixed some type of column in User database

--- laravel/database/migrations/0001_01_01_000000_create_users_table.php
+++ laravel/database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,9 +14,9 @@ return new class extends Migration
 		Schema::create("users", function(Blueprint $table)
 		{
 			$table->string("userid")->primary();
-			$table->string("username");
+			$table->text("username");
 			$table->string("useridnumber")->unique();
-			$table->string("userrole");
+			$table->text("userrole");
 			$table->string("password");
 			$table->timestamps();
 		});
