5a0dc3f6735d5e3460fc740c44a68b391a2a5e14
McArroy <132247939+McArroy@users.noreply.github.com>
2025-08-12 08:32:05 +0700

    Fixed authentication warning status on login page

--- laravel/app/Providers/FortifyServiceProvider.php
+++ laravel/app/Providers/FortifyServiceProvider.php
@@ -2,13 +2,16 @@
 
 namespace App\Providers;
 
+use App\Traits\DeterministicEncryption;
 use App\Actions\Fortify\CreateNewUser;
 use App\Actions\Fortify\ResetUserPassword;
 use App\Actions\Fortify\UpdateUserPassword;
 use App\Actions\Fortify\UpdateUserProfileInformation;
+use App\Models\User;
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Str;
@@ -20,6 +23,8 @@ use Laravel\Fortify\Fortify;
 
 class FortifyServiceProvider extends ServiceProvider
 {
+	use DeterministicEncryption;
+
 	/**
 	 * Register any application services.
 	 */
@@ -38,6 +43,18 @@ class FortifyServiceProvider extends ServiceProvider
 		Fortify::updateUserPasswordsUsing(UpdateUserPassword::class);
 		Fortify::resetUserPasswordsUsing(ResetUserPassword::class);
 		Fortify::redirectUserForTwoFactorAuthenticationUsing(RedirectIfTwoFactorAuthenticatable::class);
+
+		Fortify::authenticateUsing(function(Request $request)
+		{
+			$user = User::where("useridnumber", $this->encryptDeterministic(strtolower(trim($request->useridnumber))))->first();
+
+			if ($user && Hash::check($request->password, $user->password))
+				return $user;
+
+			session()->flash("toast_info", "ID Pengguna atau Password Salah!");
+
+			return null;
+		});
 
 		RateLimiter::for("login", function(Request $request)
 		{
