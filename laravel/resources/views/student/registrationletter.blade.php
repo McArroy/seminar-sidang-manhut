a45947e98e7e7844f0c865cd459a8cb6158f1d72
McArroy <132247939+McArroy@users.noreply.github.com>
2025-08-25 11:08:09 +0700

    Fixed display re-preview letters by not using the same display when letters created in the first place

--- laravel/resources/views/student/registrationletter.blade.php
+++ laravel/resources/views/student/registrationletter.blade.php
@@ -3,26 +3,33 @@
 		<link rel="stylesheet" href="/assets/css/pages/registrationform.css?v=1.0">
 	@endsection
 
-	<x-slot name="title">Form Pendaftaran</x-slot>
+	@if (isset($_GET["mod"]) && $_GET["mod"] === "preview")
+	@php $IsPreviewMode = true; @endphp
+	@else
+	@php $IsPreviewMode = false; @endphp
+	@endif
+
+	<x-slot name="title">{{ $IsPreviewMode ? "Pratinjau Formulir" : "Form Pendaftaran" }}</x-slot>
 	<x-slot name="icon">basil:document-outline</x-slot>
 
 	@if ($_GET["type"] === "seminar")
 
 	@php $IsThesisDefense = false; @endphp
 
-	<x-slot name="pagetitle">Form Pendafataran Seminar</x-slot>
-
 	@elseif ($_GET["type"] === "thesisdefense")
 
 	@php $IsThesisDefense = true; @endphp
 
-	<x-slot name="pagetitle">Form Pendafataran Sidang Akhir</x-slot>
-
 	@endif
 
+	<x-slot name="pagetitle">{{ $IsPreviewMode ? "Pratinjau Formulir" : "Form Pendaftaran" }} {{ $IsThesisDefense ? "Sidang Akhir" : "Seminar" }}</x-slot>
+
 	<div class="letter">
-		<p>Berkas form pendaftaran {{ $IsThesisDefense ? "sidang akhir" : "seminar" }} berhasil dibuat, harap <span>download</span> file berikut dan <span>lengkapi tanda tangan yang tertera</span>.</p>
-		<p>Setelah berkas form pendaftaran {{ $IsThesisDefense ? "sidang akhir" : "seminar" }} ditandatangani, <span>upload kembali</span> di Menu <span>Persyaratan {{ $IsThesisDefense ? "Sidang" : "Seminar" }}</span> bersama berkas pendukung lainnya.</p>
+		@if (!$IsPreviewMode)
+		<p>Berkas form pendaftaran {{ $IsThesisDefense ? "Sidang Akhir" : "Seminar" }} berhasil dibuat, harap <span>unduh</span> file berikut dan <span>lengkapi tanda tangan yang tertera</span>.</p>
+		<p>Setelah berkas form pendaftaran {{ $IsThesisDefense ? "Sidang Akhir" : "Seminar" }} ditandatangani, <span>unggah kembali</span> di Menu <a href="{{ route('student.requirements', ['type' => $IsThesisDefense ? 'thesisdefense' : 'seminar']) }}">Persyaratan {{ $IsThesisDefense ? "Sidang" : "Seminar" }}</a> bersama berkas pendukung lainnya.</p>
+		@endif
+
 		<x-letter :data="$data" :IsThesisDefense="$IsThesisDefense"></x-letter>
 	</div>
 </x-app-layout>
