3084a758777c672751b61b7dcfa02a3d3f32d78c
McArroy <132247939+McArroy@users.noreply.github.com>
2025-08-26 15:58:53 +0700

    Added Letter database

---
+++ laravel/database/migrations/2025_08_25_072033_create_letters_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+	/**
+	 * Run the migrations.
+	 */
+	public function up() : void
+	{
+		Schema::create("letters", function(Blueprint $table)
+		{
+			$table->string("letterid")->primary();
+			$table->text("academicid");
+			$table->text("letternumber")->unique();
+			$table->text("moderator");
+			$table->date("letterdate");
+			$table->text("supervisory_committee")->nullable();
+			$table->text("external_examiner")->nullable();
+			$table->text("chairman_session")->nullable();
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 */
+	public function down() : void
+	{
+		Schema::dropIfExists("letters");
+	}
+};
